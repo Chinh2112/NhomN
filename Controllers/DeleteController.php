4c4fb29f0ef5fb054dcb539f50c845cac771f6a3
chinh <19211tt4502@mai;.tdc.edu.vn>
2022-06-03 00:44:08 +0700

    update3

--- Controllers/DeleteController.php
+++ Controllers/DeleteController.php
@@ -18,18 +18,5 @@ class DeleteController extends Controller
         return redirect('products')->with('alertDel', $alertDel);
     }
 
-    public function destroyProtype($id)
-    {
-        $obj = new Protypes();
-        $obj->where('protype_id', $id)->delete();
-        $alertDel = 'Delete Protype Type Success !!! ';
-        return redirect('protypes')->with('alertDel', $alertDel);
-    }
-    public function destroyCategory($id)
-    {
-        $obj = new Categories();
-        $obj->where('category_id', $id)->delete();
-        $alertDel = 'Delete Category Type Success !!! ';
-        return redirect('category')->with('alertDel', $alertDel);
-    }
+   
 }
