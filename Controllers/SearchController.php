350ab8c5506b866c0fd415a0c49331f8802db113
Pham Hung Cuong <cuongp948@gmail.com>
2022-06-03 22:00:05 +0700

    tìm kiem

--- Controllers/SearchController.php
+++ Controllers/SearchController.php
@@ -11,9 +11,9 @@ class SearchController extends Controller
     public function search(Request $request){
         $per_page = $request->get('per_page');
         $obj = new Products();
-         $name = $request->get('name');
+        $name = $request->get('name');
         $products = $obj->where('product_name','like',"%$name%")->paginate($per_page);
-          $products->appends(['name' => $name]);
+        $products->appends(['name' => $name]);
         return view('search',['products' => $products]);
     }
 }
