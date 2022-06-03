90f727086967fa31264e641badfdba603dfa2f97
Pham Hung Cuong <cuongp948@gmail.com>
2022-06-03 22:13:56 +0700

    thêm

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -121,7 +121,47 @@
                 </form>
             </td>
         </tr>
-      
+        <!-- Edit -->
+        <div class="modal fade form-edit" id="formEditCompany{{$product->product_id}}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form action="{{ route ('editProducts.update',$product->product_id)}}" method="post"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Edit Products</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <h5>Product Name</h5>
+                                <input type="text" class="form-control" name="edit-name"
+                                    value="{{$product->product_name}}" required>
+                                <h5>Product Price</h5>
+                                <input type="text" name="edit-web" class="form-control"
+                                    value="{{$product->product_price}}" id="" required>
+                                <h5>Product Image </h5>
+                                <input type="file" name="image" class="form-control" value="{{$product->product_image}}"
+                                    id="" required>
+                                <h5>Protype Id</h5>
+                                <input type="text" name="edit-code" class="form-control"
+                                    value="{{$product->trainer_id}}" id="" required>
+                                <h5>Product Description</h5>
+                                <input type="tel" name="edit-phone" class="form-control"
+                                    value="{{$product->product_description}}" id="" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <input type="submit" value="Save" class="btn btn-success" name="submitEdit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
         @endforeach
     </tbody>
 </table>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProtypeController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\CustomAuthController;
-
+use App\Http\Controllers\DeleteController;
+use App\Http\Controllers\EditingController;
 
 
 Route::get('/', function () {
@@ -11,6 +15,16 @@ Route::get('/', function () {
 });
 Route::get('products',[ProductsController::class,'getProducts'])->middleware('per_page');
 Route::get('products',[ProductsController::class,'index']);
+Route::get('protypes',[ProtypeController::class,'getProtype'])->middleware('per_page');
+Route::get('search',[SearchController::class,'search'])->middleware('per_page');
+
+Route::get('category',[CategoriesController::class,'getCategory']);
+//Add Category
+Route::post('addCategory', [CategoriesController::class, 'addCategory'])->name('addCategory');
+// Edit Category
+Route::post('editCategory/{id}', [CategoriesController::class, 'editCategory'])->name('editCategory.update');
+// Delete Category
+Route::delete('categories/{id}', [DeleteController::class, 'destroyCategory']);
 
 
 // Route:
@@ -22,7 +36,18 @@ Route::get('registration', [CustomAuthController::class, 'registration'])->name(
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 
-
+// Delete Product
+Route::delete('products/{id}', [DeleteController::class, 'destroy']);
 //Add Product
 Route::post('addProducts', [ProductsController::class, 'addProducts'])->name('addProducts');
+//Edit Product
+Route::post('editProducts/{id}', [ProductsController::class, 'editProducts'])->name('editProducts.update');
 
+
+
+// Delete Product Type
+Route::delete('protypes/{id}', [DeleteController::class, 'destroyProtype']);
+//Edit Product Type
+Route::post('editProtype/{id}', [ProtypeController::class, 'editProtype'])->name('editProtype.update');
+//Add Product Type
+Route::post('addProtype', [ProtypeController::class, 'addProtype'])->name('addProtype');
