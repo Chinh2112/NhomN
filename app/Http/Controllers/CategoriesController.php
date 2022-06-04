d8efe9784e7d02bcb4f793972d4f2c0de3e8b620
Pham Hung Cuong <cuongp948@gmail.com>
2022-06-04 21:49:38 +0700

    update tim kiem danh muc

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -14,38 +14,16 @@ class CategoriesController extends Controller
 {
     //
     public function getCategory(Request $request){
+     
+
         $per_page = $request->input('per_page');
         $obj = new Categories();
         $category = $obj->paginate($per_page);
-        return view('category',['categories' => $category]);
-        // $companies =  DB::table('companies')->simplePaginate(5);
-        // return view('companies',['companies' =>  $companies]);
+        $name = $request->get('search');
+        $category = $obj->where('category_name', 'like', "%$name%")->orderBy('category_id', 'DESC')->paginate($per_page);
+        $category->appends(['search' => $name]);
+        return view('category', ['categories' => $category]);
+
     }
-    protected function addCategory(Request $request)
-    {
-        if ($request->isMethod('POST')) {
-            $name = $request->name; 
-          
-            
-            Categories::create([
-                'category_name' => $name,          
-                'timestamp' => now()            
-            ]);
-            $alertAdd = 'Add Product Type Success !!! ';
-            return redirect('category')->with('alertAdd', $alertAdd);
-        } else {
-            return redirect('/category');
-        }
-    }
-    protected function editCategory( Request $request,$id)
-    {
-        $obj = new Categories();
-        $data = $request->all();
-        $category = $obj->find($id);
-        $category->category_name = $data['edit-name'];
-        $category->updated_at = now();
-        $category->save();
-        $alertEdit = 'Edit Product Type Success !!! ';
-        return redirect('/category')->with('alertEdit',$alertEdit);
-    }
+
 }
